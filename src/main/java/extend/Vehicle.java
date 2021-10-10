88afad7464c809ec2b0bc48f84972a52741ddd74
m.hejazi <mahsa.hejazii@gmai.com>
2021-10-10 10:07:10 +0330

    extend

---
+++ src/main/java/extend/Vehicle.java
@@ -0,0 +1,9 @@
+package extend;
+
+public class Vehicle {
+    protected String brand = "BMW";         // Vehicle attribute
+    public void honk() {                     // Vehicle method
+        System.out.println("BMW, M3!");
+    }
+
+}
